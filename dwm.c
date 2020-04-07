f5431dcc6156b98f3df2190cbdc8b86224229293
i-8086 <mighty8086@gmail.com>
2020-04-07 18:59:59 +0000

    autostart patch

--- dwm.c
+++ dwm.c
@@ -249,6 +249,7 @@ static void resizerequest(XEvent *e);
 static void restack(Monitor *m);
 static void rotatestack(const Arg *arg);
 static void run(void);
+static void runAutostart(void);
 static void scan(void);
 static int sendevent(Window w, Atom proto, int m, long d0, long d1, long d2, long d3, long d4);
 static void sendmon(Client *c, Monitor *m);
@@ -1774,6 +1775,12 @@ run(void)
 }
 
 void
+runAutostart(void) {
+	system("cd ~/.dwm; ./autostart_blocking.sh");
+	system("cd ~/.dwm; ./autostart.sh &");
+}
+
+void
 scan(void)
 {
 	unsigned int i, num;
@@ -2836,13 +2843,15 @@ main(int argc, char *argv[])
 		die("dwm: cannot open display");
 	checkotherwm();
         XrmInitialize();
-        loadxrdb();
 	setup();
 #ifdef __OpenBSD__
 	if (pledge("stdio rpath proc exec", NULL) == -1)
 		die("pledge");
 #endif /* __OpenBSD__ */
 	scan();
+	runAutostart();
+	loadxrdb();
+	xrdb(NULL);
 	run();
 	cleanup();
 	XCloseDisplay(dpy);
